8692167f8a3ac6568c9a520b6664ddaf9b31c55d
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-17 13:23:47 +0200

    updated several items

--- WGAN/model.py
+++ WGAN/model.py
@@ -1,85 +1,99 @@
+"""
+Discriminator and Generator implementation from DCGAN paper,
+with removed Sigmoid() as output from Discriminator (and therefor
+it should be called critic)
+"""
+
 import torch
 import torch.nn as nn
 
-class Critic(nn.Module):
-    def __init__(self, channels_img, features_d): #features d are gonna be the channels that are going to change as we go through the layers of the discriminator
-        super(Critic, self).__init__()
-        #in the paper they mention that they don't use Batchnorm in neither first layer of discriminator nor last layer of the generator
-        self.disc = nn.Sequential(
-            #input: N x channels_img x 64 x 64
-      		nn.Conv2d(
-				channels_img, features_d, kernel_size=4, stride=2, padding=1
-			),
-			nn.LeakyReLU(0.2),
-			self._block(features_d, features_d * 2, 4, 2, 1), #16x16
-			self._block(features_d * 2, features_d * 4, 4, 2, 1), #8x8
-   			self._block(features_d * 4, features_d * 8, 4, 2, 1), #4x4
-			nn.Conv2d(features_d*8, 1, kernel_size=4, stride=2, padding=0), #1x1 (a single value representing if the img is fake or real)
-  		)	
-        
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-			nn.Conv2d(
-       			in_channels,
-          		out_channels,
-          		kernel_size,
-          		stride,
-          		padding,
-				bias=False,
-            ),
-			nn.BatchNorm2d(out_channels),
-			nn.LeakyReLU(0.2),
-		)
-    
-    def forward(self, x):
-    	return self.disc(x)
- 
-class Generator(nn.Module):
-    def __init__(self, z_dim, channels_img, features_g):
-        super(Generator, self).__init__()
-        self.gen = nn.Sequential(
-			self._block(z_dim, features_g * 16, 4, 1, 0), # N x f_g * 16 x 4 x 4
-			self._block(features_g * 16, features_g * 8, 4, 2, 1), # 8 x 8
-			self._block(features_g * 8, features_g * 4, 4, 2, 1), # 16 x 16
-			self._block(features_g * 4, features_g * 2, 4, 2, 1), # 32 x 23
-			nn.ConvTranspose2d(
-				features_g*2, channels_img, kernel_size=4, stride=2, padding=1,
-			),
-			nn.Tanh(), # [-1, 1] range
-		)
-    
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-			nn.ConvTranspose2d(
-       			in_channels,
-          		out_channels,
-          		kernel_size,
-          		stride,
-          		padding,
-				bias=False,				
-         	),
-			nn.BatchNorm2d(out_channels),
-			nn.ReLU(),
 
-		)
+class Discriminator(nn.Module):
+    def __init__(self, channels_img, features_d):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            # input: N x channels_img x 64 x 64
+            nn.Conv2d(
+                channels_img, features_d, kernel_size=4, stride=2, padding=1
+            ),
+            nn.LeakyReLU(0.2),
+            # _block(in_channels, out_channels, kernel_size, stride, padding)
+            self._block(features_d, features_d * 2, 4, 2, 1),
+            self._block(features_d * 2, features_d * 4, 4, 2, 1),
+            self._block(features_d * 4, features_d * 8, 4, 2, 1),
+            # After all _block img output is 4x4 (Conv2d below makes into 1x1)
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.InstanceNorm2d(out_channels, affine=True),
+            nn.LeakyReLU(0.2),
+        )
+
     def forward(self, x):
-    	return self.gen(x)
+        return self.disc(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, channels_noise, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.net = nn.Sequential(
+            # Input: N x channels_noise x 1 x 1
+            self._block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
+            self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
+            nn.ConvTranspose2d(
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
+            ),
+            # Output: N x channels_img x 64 x 64
+            nn.Tanh(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 
 def initialize_weights(model):
+    # Initializes weights according to the DCGAN paper
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
-            
+
+
 def test():
     N, in_channels, H, W = 8, 3, 64, 64
-    z_dim = 100
+    noise_dim = 100
     x = torch.randn((N, in_channels, H, W))
-    disc = Critic(in_channels, 8)
-    initialize_weights(disc)
-    assert disc(x).shape == (N, 1, 1, 1)
-    gen = Generator(z_dim, in_channels, 8)
-    initialize_weights(gen)
-    z= torch.randn((N, z_dim, 1, 1))
-    assert gen(z).shape == (N, in_channels, H, W)
-    print("SUCCESS")
-test()
+    disc = Discriminator(in_channels, 8)
+    assert disc(x).shape == (N, 1, 1, 1), "Discriminator test failed"
+    gen = Generator(noise_dim, in_channels, 8)
+    z = torch.randn((N, noise_dim, 1, 1))
+    assert gen(z).shape == (N, in_channels, H, W), "Generator test failed"
+    print("Success, tests passed!")
+
+if __name__ == "__main__":
+    test()

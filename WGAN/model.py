a9445bcea49274fe1ac76d37ac70ab3712013f7b
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-17 13:44:44 +0200

    Update model.py

--- WGAN/model.py
+++ WGAN/model.py
@@ -3,87 +3,78 @@ Discriminator and Generator implementation from DCGAN paper,
 with removed Sigmoid() as output from Discriminator (and therefor
 it should be called critic)
 """
-
 import torch
 import torch.nn as nn
 
-
+# Discriminator implementation
 class Discriminator(nn.Module):
     def __init__(self, channels_img, features_d):
         super(Discriminator, self).__init__()
+
+        # Main discriminator network
         self.disc = nn.Sequential(
-            # input: N x channels_img x 64 x 64
-            nn.Conv2d(
-                channels_img, features_d, kernel_size=4, stride=2, padding=1
-            ),
+            # Initial convolution layer
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2),
-            # _block(in_channels, out_channels, kernel_size, stride, padding)
+
+            # Stacked convolutional blocks
             self._block(features_d, features_d * 2, 4, 2, 1),
             self._block(features_d * 2, features_d * 4, 4, 2, 1),
             self._block(features_d * 4, features_d * 8, 4, 2, 1),
-            # After all _block img output is 4x4 (Conv2d below makes into 1x1)
+
+            # Final convolution layer to produce single value output
             nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
         )
 
+    # Convolutional block for discriminator
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
-            nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-                bias=False,
-            ),
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.InstanceNorm2d(out_channels, affine=True),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2)
         )
 
     def forward(self, x):
         return self.disc(x)
 
-
+# Generator implementation
 class Generator(nn.Module):
     def __init__(self, channels_noise, channels_img, features_g):
         super(Generator, self).__init__()
+
+        # Main generator network
         self.net = nn.Sequential(
-            # Input: N x channels_noise x 1 x 1
-            self._block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
-            self._block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
-            self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
-            self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
-            nn.ConvTranspose2d(
-                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
-            ),
-            # Output: N x channels_img x 64 x 64
+            # Initial deconvolution layer
+            self._block(channels_noise, features_g * 16, 4, 1, 0),
+            # Stacked deconvolutional blocks to upscale image
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),
+            self._block(features_g * 8, features_g * 4, 4, 2, 1),
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),
+
+            # Final deconvolution layer to produce image with desired channels
+            nn.ConvTranspose2d(features_g * 2, channels_img, kernel_size=4, stride=2, padding=1),
+            # Using Tanh to get output in range [-1, 1]
             nn.Tanh(),
         )
 
+    # Deconvolutional block for generator
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels,
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-                bias=False,
-            ),
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
+            nn.ReLU()
         )
 
     def forward(self, x):
         return self.net(x)
 
-
+# Function to initialize weights as per DCGAN paper
 def initialize_weights(model):
-    # Initializes weights according to the DCGAN paper
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
 
-
+# Function to test the models
 def test():
     N, in_channels, H, W = 8, 3, 64, 64
     noise_dim = 100
@@ -97,3 +88,4 @@ def test():
 
 if __name__ == "__main__":
     test()
+

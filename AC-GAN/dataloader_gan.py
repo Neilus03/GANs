0f5acbc39a21756c897b9d7fe84a5f18aba339c2
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-10-05 10:33:00 +0200

    Update dataloader_gan.py

--- AC-GAN/dataloader_gan.py
+++ AC-GAN/dataloader_gan.py
@@ -1,26 +1,28 @@
 from torch.utils.data import Dataset
 from PIL import Image
-import pandas as pd
 import os
 import torch
 from torchvision import transforms
 
 class EGDDataset(Dataset):
-    def __init__(self, image_folder, label_file, transform=None):
-        self.labels = pd.read_excel(label_file)
-        self.image_folder = image_folder
+    def __init__(self, root_folder, transform=None):
+        self.root_folder = root_folder
         self.transform = transform
+        self.file_list = []
+        self.label_list = []
+        for label in os.listdir(root_folder):
+            for filename in os.listdir(os.path.join(root_folder, label)):
+                self.file_list.append(os.path.join(root_folder, label, filename))
+                self.label_list.append(int(label))
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.file_list)
 
     def __getitem__(self, idx):
-        # Image loading
-        img_name = os.path.join(self.image_folder, f"{idx+1}.BMP") 
-        image = Image.open(img_name)
+        img_path = self.file_list[idx]
+        image = Image.open(img_path)
 
-        # Labels
-        final_label = torch.tensor(self.labels.iloc[idx, -1], dtype=torch.long)  # The last column (FINAL)
+        label = torch.tensor(self.label_list[idx], dtype=torch.long)
 
         if self.transform:
             image = self.transform(image)
@@ -28,7 +30,8 @@ class EGDDataset(Dataset):
         # Flatten the image
         image = torch.flatten(image)
 
-        return image, final_label
+        return image, label
+
 
 # Example usage
 if __name__ == "__main__":
@@ -37,8 +40,8 @@ if __name__ == "__main__":
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    dataset = EGDDataset(image_folder="/content/drive/MyDrive/EGD-Barcelona/merged/all_images_mean_cropped", 
-                         label_file="/content/drive/MyDrive/EGD-Barcelona/merged/full_well.xlsx", 
-                         transform=transform)
+    dataset = EGDDataset(root_folder="/content/drive/MyDrive/EGD-Barcelona/split_by_label/train", 
+                     transform=transform)
+
     
     print(dataset[2])  # Output will be (image, final_label)
